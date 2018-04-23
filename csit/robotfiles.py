d0d15f0389e1cbcbdf53792b8c4328c1c6c3e386
Sam Hague <shague@redhat.com>
2018-04-23 15:14:42 -0400

    refactor tests
    Signed-off-by: Sam Hague <shague@redhat.com>

--- csit/robotfiles.py
+++ csit/robotfiles.py
@@ -3,34 +3,28 @@ import os
 import re
 import xml.etree.cElementTree as ET
 from subprocess import Popen
+from ovs import flows
 
-from ovs.flows import Flows
 
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("csit.robotfiles")
 
 
 class RobotFiles:
-    JOBTAG = "job"
-    TMP = "/tmp"
+    OUTDIR = "/tmp/robotjob"
     CHUNK_SIZE = 65536
     DUMP_FLOWS = "sudo ovs-ofctl dump-flows br-int -OOpenFlow13"
+    TMP = "/tmp"
 
-    def __init__(self, infile, outdir, jobtag):
+    def __init__(self, infile, outdir):
         if outdir is None:
-            outdir = RobotFiles.TMP
-        if jobtag is None:
-            jobtag = RobotFiles.JOBTAG
-        self.outdir = "{}/{}".format(outdir, jobtag)
+            self.outdir = RobotFiles.TMP
+        else:
+            self.outdir = outdir
         self.datafilepath = infile
         self.pdata = {}
         self.re_normalize_text = re.compile(r"( \n)|(\[A\[C.*)")
+        # uri=restconf/config/interface-service-bindings:service-bindings, headers=None json=None</msg>
         self.re_uri = re.compile(r"uri=(?P<uri>.*),")
-        self.re_model = re.compile(r"uri=(?P<uri>.*),")
-
-    def set_log_level(self, level):
-        logger.setLevel(level)
 
     def gunzip_output_file(self):
         infile = self.datafilepath
@@ -45,7 +39,7 @@ class RobotFiles:
             if not os.path.isdir(path):
                 raise
 
-    def mkdir_job_path(self):
+    def mk_outdir(self):
         self.mkdir(self.outdir)
 
     def read_chunks(self, fp):
@@ -61,9 +55,9 @@ class RobotFiles:
         with open(self.datafilepath, 'rb') as fp:
             for chunk in self.read_chunks(fp):
                 for m in re_st.finditer(chunk):
-                    print('%02d-%02d: %s' % (m.start(), m.end(), m.group(0)))
+                    logger.info("%02d-%02d: %s", m.start(), m.end(), m.group(0))
                     cnt += 1
-        print "total matches: {}".format(cnt)
+        logger.info("total matches: %d", cnt)
 
     class State:
         def __init__(self):
@@ -91,12 +85,16 @@ class RobotFiles:
     def print_config(self):
         logger.info("datafilepath: %s, outdir: %s", self.datafilepath, self.outdir)
 
+    # scan until test id= is seen. This indicates the start of a new test -> state=test
+    # - scan until Get Test Teardown Debugs -> state=debugs
+    #   - scan until Get DumpFlows And Ovsconfig -> state=nodes
+    #   - scan until Get Model Dump -> state=models
     def process_element(self, state, event, element):
         tag = element.tag
         text = element.text
         attribs = element.attrib
-        if logger.isEnabledFor(logging.DEBUG):
-            logger.debug("%s - %s - %s - %s - %s - %s", state.state, state.command, event, tag, (text is not None), attribs)
+        if logger.isEnabledFor(logging.DEBUG) and text is not None and attribs:
+            logger.debug("process_element: %s - %s - %s - %s - %s - %s", state.state, state.command, event, tag, (text is not None), attribs)
         if event == "end":
             if element.tag == "test":
                 state.pdata['nodes'] = state.nodes
@@ -109,31 +107,38 @@ class RobotFiles:
 
         if event == "start" and state.state == "init":
             # <test id="s1-s1-s1-t1" name="Create VLAN Network (l2_network_1)">
+            # <test id="s1-t1" name="Create VLAN Network net_1">
             if element.tag == "test":
                 state.test_id = element.get("id")
                 state.pdata["name"] = element.get("name")
                 state.state = "test"
+                state.command = ""
         elif event == "start" and state.state == "test":
             # <kw type="teardown" name="Get Test Teardown Debugs" library="OpenStackOperations">
             if element.tag == "kw" and element.get("name") == "Get Test Teardown Debugs":
                 state.state = "debugs"
+                state.command = ""
         elif event == "start" and state.state == "debugs":
             # <arg>Get DumpFlows And Ovsconfig</arg>
-            if element.tag == "arg" and element.text == "Get DumpFlows And Ovsconfig":
+            if element.tag == "kw" and element.get("name") == "Get DumpFlows And Ovsconfig":
                 state.state = "nodes"
+                state.command = ""
             # <arg>Get Model Dump</arg>
-            if element.tag == "arg" and element.text == "Get Model Dump":
+            if element.tag == "kw" and element.get("name") == "Get Model Dump":
                 state.state = "models"
+                state.command = ""
         elif event == "start" and state.state == "nodes":
             # <arg>${OS_CONTROL_NODE_IP}</arg>
             if element.tag == "arg" and element.text is not None and "${OS_" in element.text:
                 state.node = element.text[element.text.find("{") + 1:element.text.find("}")]
                 state.nodes[state.node] = {}
                 state.state = "nodes2"
+                state.command = ""
         elif event == "start" and state.state == "nodes2":
             # <kw name="Write Commands Until Expected Prompt" library="Utils">
             if element.tag == "kw" and element.get("name") == "Write Commands Until Expected Prompt":
                 state.state = "kw"
+                state.command = ""
         elif event == "start" and state.state == "kw":
             # <arg>ip -o link</arg>
             if element.tag == "arg" and element.text is not None:
@@ -153,9 +158,10 @@ class RobotFiles:
                     # [jenkins@rele ^Mng-36618-350-devstack-newton-0 ~]&gt; ip -o link</msg>
                     if text.find("jenkins") != -1:
                         state.state = "nodes2"
+                        state.command = ""
                     else:
-                        state.command = text
                         state.state = "msg"
+                        state.command = text
         elif state.state == "msg":
             # <msg timestamp="20170414 07:31:21.786" level="INFO">
             if element.tag == "msg" and element.text is not None:
@@ -164,6 +170,7 @@ class RobotFiles:
                 # this command is the last one
                 if state.command == "sudo ovs-ofctl dump-group-stats br-int -OOpenFlow13":
                     state.state = "debugs"
+                    state.command = ""
                 else:
                     # still more debugs for this node
                     state.state = "nodes2"
@@ -173,8 +180,8 @@ class RobotFiles:
             if element.tag == "msg" and element.text is not None and element.text.find("uri") != -1:
                 uri = self.re_uri.search(element.text)
                 if uri is not None and "uri" in uri.group():
-                    state.command = uri.group("uri")
                     state.state = "uri"
+                    state.command = uri.group("uri")
         elif event == "start" and state.state == "models":
             # <kw type="foritem" name="${model} = config/neutronvpn:router-interfaces-map">
             if element.tag == "kw" and "name" in element.attrib:
@@ -183,18 +190,22 @@ class RobotFiles:
                 if len(name_split) == 2:
                     model = name_split[1]
                 if model is not None:
-                    state.command = model
                     state.state = "uri"
+                    state.command = model
         elif state.state == "uri":
             if element.tag == "msg" and element.text is not None and element.text.find("pretty_output") != -1:
                 state.state = "dump"
+                # do not clear the state.command
         elif state.state == "dump":
             if element.tag == "msg" and element.text is not None:
                 state.models[state.command] = element.text
-                if state.command == "restconf/operational/rendered-service-path:rendered-service-path":
+                # if state.command == "restconf/operational/rendered-service-path:rendered-service-path":
+                if state.command == "restconf/operational/opendaylight-inventory:nodes":
                     state.state = "done"
+                    state.command = ""
                 else:
                     state.state = "models"
+                    state.command = ""
 
     def parse_xml_data_file(self):
         state = self.State()
@@ -204,8 +215,9 @@ class RobotFiles:
             for event, element in iterparser:
                 self.process_element(state, event, element)
                 element.clear()
-                # if "s1-s1-s1-t1" in self.pdata:
-                #    break
+                # debugging code to stop after the named test case is processed
+                if "s1-t1" in self.pdata:
+                    break
             root.clear()
 
     def write_pdata(self):
@@ -223,7 +235,7 @@ class RobotFiles:
             mdir = tdir + "/models"
             self.mkdir(mdir)
             for mindex, (model, mdata) in enumerate(test['models'].items()):
-                filename = mdir + "/" + self.fix_model_name(model) + ".txt"
+                filename = mdir + "/" + self.fix_model_name(model) + ".json"
                 with open(filename, 'w') as fp:
                     if mdata is not None:
                         fp.writelines(mdata)
@@ -235,14 +247,42 @@ class RobotFiles:
                 ndir = tdir + "/" + nodeid
                 if RobotFiles.DUMP_FLOWS not in node:
                     continue
-                filename = ndir + "/" + self.fix_command_names(RobotFiles.DUMP_FLOWS) + ".f.txt"
+                filename = ndir + "/" + self.fix_command_names(RobotFiles.DUMP_FLOWS)
                 logger.info("Processing: %s", filename)
+                filename = filename + ".f.txt"
                 dump_flows = node[RobotFiles.DUMP_FLOWS]
-                flows = Flows(dump_flows, logging.DEBUG)
-                flows.write_fdata(filename)
+                fls = flows.Flows(dump_flows)
+                fls.write_fdata(filename)
 
     def fix_command_names(self, cmd):
         return cmd.replace(" ", "_")
 
     def fix_model_name(self, model):
         return model.replace("/", "_")
+
+
+def run(args):
+    robotfile = RobotFiles(args.infile, args.outdir)
+    robotfile.print_config()
+    robotfile.mk_outdir()
+    if args.gunzip:
+        robotfile.gunzip_output_file()
+    robotfile.print_config()
+    robotfile.parse_xml_data_file()
+    robotfile.write_pdata()
+    if args.dump:
+        robotfile.write_debug_pdata()
+
+
+def add_parser(subparsers):
+    parser = subparsers.add_parser("csit")
+    parser.add_argument("infile",
+                        help="XML output from a Robot test, e.g. output_01_l2.xml.gz")
+    parser.add_argument("outdir",
+                        help="the directory that the parsed data is written into")
+    parser.add_argument("-g", "--gunzip", action="store_true",
+                        help="unzip the infile")
+    parser.add_argument("-d", "--dump", action="store_true",
+                        help="dump extra debugging, e.g. ovs metadata")
+    parser.set_defaults(func=run)
+
---
+++ netvirt/constants.py
@@ -0,0 +1,78 @@
+VIF_TYPE_TO_PREFIX = {
+                      'ovs':'tap',
+                      'vhost_user':'vhu'
+                      }
+
+VIF_TYPE = 'neutron-binding:vif-type'
+IFACE_PARENT = 'odl-interface:parent-interface'
+VIF_TYPE = 'neutron-binding:vif-type'
+IFTYPE_VLAN = 'iana-if-type:l2vlan'
+IFTYPE_TUNNEL = 'iana-if-type:tunnel'
+
+NODE_GROUP = 'flow-node-inventory:group'
+NODE_TABLE = 'flow-node-inventory:table'
+
+DSM_FILE = 0
+DSM_DSTYPE = 1
+DSM_PATH = 2
+DSM_ROOT1 = 3
+DSM_ROOT2 = 4
+"""
+   source map to fetch data from data store.
+   Format is:
+   resource-key-name:[filename,datastore_type,resource-url,container-name,list-title]
+"""
+DSMAP = {
+    'bindings': ['service-bindings.json', 'config',
+                 'interface-service-bindings:service-bindings',
+                 'service-bindings', 'services-info'],
+    'dpnendpoints': ['dpn-endpoints.json', 'config', 'itm-state:dpn-endpoints',
+                     'dpn-endpoints', 'DPN-TEPs-info'],
+    'elaninstances': ['elan-instances.json', 'config', 'elan:elan-instances',
+                      'elan-instances', 'elan-instance'],
+    'elaninterfaces': ['elan-interfaces.json', 'config', 'elan:elan-interfaces',
+                       'elan-interfaces', 'elan-interface'],
+    'fibentries': ['fibentries.json', 'config', 'odl-fib:fibEntries',
+                   'fibEntries', 'vrfTables'],
+    'idpools': ['idpools.json', 'config', 'id-manager:id-pools',
+                      'id-pools', 'id-pool'],
+    'ifconfig': ['iface-config.json', 'config', 'ietf-interfaces:interfaces',
+                 'interfaces', 'interface'],
+    'itmconfig': ['itm-config.json', 'config', 'itm-state:dpn-teps-state',
+                  'dpn-teps-state', 'dpns-teps'],
+    'ifindexes': ['ifindexes.json', 'operational',
+                  'odl-interface-meta:if-indexes-interface-map',
+                  'if-indexes-interface-map', 'if-index-interface'],
+    'ifstate': ['ifstate.json', 'operational',
+                'ietf-interfaces:interfaces-state',
+                'interfaces-state', 'interface'],
+    'inventory': ['inventory-config.json', 'config',
+                  'opendaylight-inventory:nodes', 'nodes', 'node'],
+    'neutronports': ['neutron-ports.json', 'config', 'neutron:neutron/ports',
+                     'ports', 'port'],
+    'neutronvpn-portip': ['neutronvpn-portip-port.json', 'config',
+                          'neutronvpn:neutron-vpn-portip-port-data',
+                          'neutron-vpn-portip-port-data',
+                          'vpn-portip-to-port'],
+    'neutrontrunks': ['neutron-trunks.json', 'config', 'neutron:neutron/trunks',
+                     'trunks', 'trunk'],
+    'tunconfig-external': ['tunnel-config-external.json', 'config',
+                           'itm-state:external-tunnel-list',
+                           'external-tunnel-list', 'external-tunnel'],
+    'tunconfig': ['tunnel-config.json', 'config', 'itm-state:tunnel-list',
+                  'tunnel-list', 'internal-tunnel'],
+    'tunstate': ['tunnel-state.json', 'operational', 'itm-state:tunnels_state',
+                 'tunnels_state', 'state-tunnel-list'],
+    'vpninstance-to-vpnid': ['vpninstance-to-vpnid.json', 'config',
+                             'odl-l3vpn:vpn-instance-to-vpn-id',
+                             'vpn-instance-to-vpn-id', 'vpn-instance'],
+    'vpninterfaces': ['vpn-interfaces.json', 'config', 'l3vpn:vpn-interfaces',
+                      'vpn-interfaces', 'vpn-interface']
+}
+
+TABLE_MAP = {
+    'ifm': [0, 17, 220],
+    'l3vpn': [19, 20, 21, 22, 36, 81],
+    'elan': [50, 51, 52, 55],
+    'acl': [211, 212, 213, 214, 215, 241, 242, 243, 244, 245]
+}
