dd8bf92eab7d8af9d252b5b40d0e049842b383d2
Sam Hague <shague@gmail.com>
2018-05-05 16:19:37 -0400

    Remove build and egg files
    Signed-off-by: Sam Hague <shague@gmail.com>

--- build/lib/odltools/monitor/cli.py
+++
@@ -1,9 +0,0 @@
-def haha(args):
-    print("tim, tim, bo-bim, banana-fana, foo-fim, tiiiimmm")
-
-
-def add_parser(parsers):
-    parser = parsers.add_parser("tim", description="timothy")
-    parser.add_argument("tim",
-                        help="say something funny!")
-    parser.set_defaults(func=haha)
