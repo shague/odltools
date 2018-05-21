adb3acab791db272c0071452149d01698c31cf57
Josh <jhershbe@redhat.com>
2018-05-21 15:47:57 +0300

    Add initial support for karaf pretty printing
    This adds a tool for reprinting karaf logs which
    pretty prints mdsal objects in the log to look
    like nicely formatted (psudo-)json.
    
    Signed-off-by: Josh <jhershbe@redhat.com>

--- odltools/cli_utils.py
+++ odltools/cli_utils.py
@@ -1,3 +1,35 @@
+# Copyright 2018 Red Hat, Inc. and others. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import logging
+import os
+
+logger = logging.getLogger("cli_utils")
+
+
+def type_input_file(path):
+    if path == '-':
+        return path
+
+    if not os.path.isfile(path):
+        logger.error('File "%s" not found' % path)
+        raise argparse.ArgumentError
+
+    return path
+
+
 def add_common_args(parser):
     parser.add_argument("--path",
                         help="the directory that the parsed data is written into")
